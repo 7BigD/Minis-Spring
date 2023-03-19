ea4716bb505fe9d93cb012b728ee0b798a95b958
yuanhaosheng03 <yuanhaosheng03@kuaishou.com>
2023-03-19 15:20:33 +0800

    IOC version 1

---
+++ src/main/java/com/yuanhaosheng/minis/context/ClassPathXmlApplicationContext.java
@@ -0,0 +1,61 @@
+package com.yuanhaosheng.minis.context;
+
+import com.yuanhaosheng.minis.bean.BeanDefinition;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ */
+
+public class ClassPathXmlApplicationContext {
+
+    private List<BeanDefinition> beanDefinitions = new ArrayList<>();
+    private Map<String, Object> singletons = new HashMap<>();
+
+    //构造器获取外部配置，解析出Bean的定义，形成内存映像
+    public ClassPathXmlApplicationContext(String fileName) {
+        this.readerXml(fileName);
+        this.instanceBeans();
+    }
+
+    private void readerXml(String fileName) {
+        SAXReader saxReader = new SAXReader();
+        try {
+            URL xmlPath = this.getClass().getClassLoader().getResource(fileName);
+            Document document = saxReader.read(xmlPath);
+            Element rootElement = document.getRootElement();
+            for (Element element : (List<Element>) rootElement.elements()) {
+                String id = element.attributeValue("id");
+                String className = element.attributeValue("class");
+                BeanDefinition beanDefinition = new BeanDefinition(id, className);
+                beanDefinitions.add(beanDefinition);
+            }
+        } catch (DocumentException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void instanceBeans() {
+        for (BeanDefinition beanDefinition : beanDefinitions) {
+            try {
+                singletons.put(beanDefinition.getId(), Class.forName(beanDefinition.getClassName()).newInstance());
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/yuanhaosheng/minis/test/AserviceImpl.java
@@ -0,0 +1,9 @@
+package com.yuanhaosheng.minis.test;
+
+/**
+ * @Description: TODO
+ * @Author: yuanhaosheng
+ * @Version: 1.0
+ */
+public class AserviceImpl {
+}
